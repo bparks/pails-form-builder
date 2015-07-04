f0e9282b5f0df9afcdaabde8a8aafdf10f22d089
Brian Parks <bparks@synapsesoftware.com>
2015-07-04 12:31:34 -0600

    Composer

--- FormBuilder.trait.php
+++ FormBuilder.trait.php
@@ -15,4 +15,29 @@ trait FormBuilder
 
 		$this->render_partial("_input_for", $options);
 	}
+
+	protected function model_options($models, $key_name, $value_name)
+	{
+		$ret = array('' => 'Choose an option');
+		foreach ($models as $item) {
+			$key = $value = '';
+
+			if (strstr($key_name, '{') === false)
+				$key = $item->$key_name;
+			else
+				$key = preg_replace_callback('/{(.*?)}/', function ($matches) use ($item) {
+					return $item->{$matches[1]};
+				}, $key_name);
+
+			if (strstr($value_name, '{') === false)
+				$value = $item->$value_name;
+			else
+				$value = preg_replace_callback('/{(.*?)}/', function ($matches) use ($item) {
+					return $item->{$matches[1]};
+				}, $value_name);
+
+			$ret[$key] = $value;
+		}
+		return $ret;
+	}
 }
